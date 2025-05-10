0c1106463ef8774b89a8ab5280e46d870bcf8ef9
Merve Esra Tekcan <esramerve.tekcan@gmail.com>
2025-05-10 21:28:30 +0300

    Movie Detail Component -UI controller

--- Frontends/MovieApi.WebApi/Controllers/MovieController.cs
+++ Frontends/MovieApi.WebApi/Controllers/MovieController.cs
@@ -1,14 +1,41 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MovieApi.Dto.Dtos.MovieDtos;
+using Newtonsoft.Json;
 
 namespace MovieApi.WebUI.Controllers;
 
 public class MovieController : Controller
 {
-    public IActionResult MovieList()
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public MovieController(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    public async Task<IActionResult> MovieList()
     {
         ViewBag.v1= "Movie List";
         ViewBag.v2 = "Main Page";
         ViewBag.v3 = "All Movies";
+
+        var client = _httpClientFactory.CreateClient();
+        var responseMessage = await client.GetAsync("https://localhost:7288/api/Movies");
+        if(responseMessage.IsSuccessStatusCode)
+        {
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values=JsonConvert.DeserializeObject<List<ResultMovieDto>>(jsonData);
+            return View(values);
+        }
+        return View();
+    }
+
+    public async Task<IActionResult> MovieDetails(int id)
+    {
+        ViewBag.v1 = "Movie List";
+        ViewBag.v2 = "Main Page";
+        ViewBag.v3 = "All Movies";
+        id = 0;
         return View();
     }
 }
